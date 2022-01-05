55a470a9bdfeba6829a440314be90fe31a1bb771
NamoZzz <95469385+NamoZzz@users.noreply.github.com>
2022-01-05 09:23:28 +0700

    Create All-Script.lua

---
+++ All-Script.lua
@@ -0,0 +1,6 @@
+local PlaceId = game.PlaceId
+
+if PlaceId == 6284583030 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/KuppaHX/MOKURO-HUB/main/PetSimulatorX.lua'))()
+elseif PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NamoZzz/All-Script/main/BloxFruits.lua"))()
